eb7b333af46c40b4293c6b10f6b914686c69de25
Eamonn Rea <eamonnrea@gmail.com>
2023-11-16 17:48:56 +0000

    Change executable name to dumpsteamcollections
    Makes more sense and is shorter.
    Also updates the build script to create
    the executable in a 'build/' directory.
    .gitignore also updated to ignore this.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 ## TODO: in future, provide other options for LevelDB
-g++ main.cpp -o get_steam_leveldb_collections -std=c++20 -lleveldb -O2
-strip get_steam_leveldb_collections
+mkdir build
+g++ main.cpp -o build/dumpsteamcollections -std=c++20 -lleveldb -O2
+strip build/dumpsteamcollections
